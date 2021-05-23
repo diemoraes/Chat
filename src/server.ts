7a4e44c2d41cc94b633d22e213f7ef0fee0e968b
diemoraes <diego_moraez@hotmail.com>
2021-05-22 23:58:07 -0300

    criando o servidor socket

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
 import "./database";
 import { routes } from "./routes";
+
 const app = express();
 
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id);
+});
 
 app.use(express.json());
 app.use(routes);
